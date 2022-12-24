f6fefb3dff63ba82d9b2fcafae84f294f568cdef
Rifqi Finaldy <rifqifinaldy@gmail.com>
2022-12-24 19:48:19 +0700

    fix import error on layouts components

--- src/layouts/layouts.tsx
+++ src/layouts/layouts.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import styles from "./layout.module.css";
+import styles from "./layouts.module.css";
 
 interface ILayouts {
   children: ReactNode;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "@/layouts/layout";
+import Layout from "@/layouts/layouts";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
