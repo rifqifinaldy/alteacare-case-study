1d1d00590a9656b194dc435ef413fc87cb7f6800
Rifqi Finaldy <rifqifinaldy@gmail.com>
2022-12-24 12:01:42 +0700

    Add Text Input Components

--- src/components/input/text-input.tsx
+++ src/components/input/text-input.tsx
@@ -1,7 +1,57 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import styles from "./text-input.module.css";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 
-const TextInput: FC = () => {
-  return <div>TextInput</div>;
+// Interface for TextInput Components Props
+interface TextInputProps {
+  id: string;
+  icon?: JSX.Element;
+  name: string;
+  placeholder?: string;
+  label?: string;
+  type: string;
+  value?: string;
+  onChange?: () => void;
+}
+
+const TextInput: FC<TextInputProps> = ({
+  id,
+  name,
+  icon,
+  placeholder,
+  label,
+  type,
+  value,
+  onChange,
+}) => {
+  return (
+    <div className={styles["input-group"]}>
+      {/* Label Input */}
+      {label && (
+        <label htmlFor={id} className={styles["input-label"]}>
+          {label}
+        </label>
+      )}
+      {/* Input Wrapper */}
+      <div className={styles["input-wrapper"]}>
+        {icon && <div className={styles["input-icon"]}>{icon}</div>}
+        {/* Actual Input HTML elements */}
+        <input
+          id={id}
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+          className={
+            `${styles["input-element"]}` +
+            " " +
+            `${icon ? "pl-10 py-1" : "pl-1 py-1"}`
+          }
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TextInput;
--- src/pages-modules/home-pages/filter-section/filter.section.tsx
+++ src/pages-modules/home-pages/filter-section/filter.section.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import styles from "./filter.module.css";
+import TextInput from "@/components/input/text-input";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 // Filter Section Component
 const FilterSection: FC = () => {
@@ -9,11 +11,19 @@ const FilterSection: FC = () => {
       <h1 className={styles["filter-section-logo"]}>Doctor Finder</h1>
       <hr className="mb-2" />
       {/* Filter Input Items */}
-      <div className={styles["filter-section-items"]}>
-        <h6>Input 1</h6>
+      <form className={styles["filter-section-items"]}>
+        {/* Search Filter */}
+        <TextInput
+          icon={<MagnifyingGlassIcon />}
+          placeholder="Search..."
+          id="search"
+          name="search"
+          type="text"
+        />
+        {/* Dropdown Filter */}
         <h6>Input 2</h6>
         <h6>Input 3</h6>
-      </div>
+      </form>
     </section>
   );
 };
